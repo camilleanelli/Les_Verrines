c4fe62b75c23159ad29930a1f822dc759728f0a6
Camille Anelli <camilleanelli32@gmail.com>
2015-04-29 15:36:23 +0000

    add a form page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   
   get 'static_page/home'
   
+  get 'static_page/form'
+  
   root 'static_page#home'
+  
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
